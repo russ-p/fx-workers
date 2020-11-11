2ba72d10473a8c646164ea9f59dbcd8abae71997
Ruslan Penkrat <penkrat@gmail.com>
2020-11-11 10:22:41 +0300

    clean deps

--- src/main/java/com/github/russ_p/fxworkers/builder/VarArg.java
+++ src/main/java/com/github/russ_p/fxworkers/builder/VarArg.java
@@ -1,11 +1,32 @@
 package com.github.russ_p.fxworkers.builder;
 
-import lombok.Value;
-
-@Value
 class VarArg<A, B, C, D> {
 	private final A a;
 	private final B b;
 	private final C c;
 	private final D d;
+
+	public VarArg(A a, B b, C c, D d) {
+		this.a = a;
+		this.b = b;
+		this.c = c;
+		this.d = d;
+	}
+
+	public A getA() {
+		return a;
+	}
+
+	public B getB() {
+		return b;
+	}
+
+	public C getC() {
+		return c;
+	}
+
+	public D getD() {
+		return d;
+	}
+
 }
